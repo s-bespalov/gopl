fe764666bda60028abb75c3a895dd0cd62a123df
Sergey Bespalov <sergey.bespalov@protonmail.com>
2020-08-18 10:50:39 +0300

    variables declaration

---
+++ ch2/ftoc/main.go
@@ -0,0 +1,13 @@
+// Ftoc prints two Fahrenheit-to-Celsius conversions
+package main
+
+import "fmt"
+
+func main() {
+	const freezingF, boilingF = 32.0, 212.0
+	fmt.Printf("%g°F = %g°C\n", freezingF, fToC(freezingF)) // "32°F = 0°C"
+}
+
+func fToC(f float64) float64 {
+	return (f - 32) * 5 / 9
+}
