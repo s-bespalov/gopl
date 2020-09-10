0e1b2223fa157aa040ea539e750fa9923e75ac06
Sergey Bespalov <sergey.bespalov@protonmail.com>
2020-09-11 00:33:11 +0300

    supersampling

--- ch3/mandelbrotColor/main.go
+++ ch3/mandelbrotColor/main.go
@@ -25,7 +25,36 @@ func main() {
 			img.Set(px, py, mandelbrot(z))
 		}
 	}
+	img = supersampling(img)
 	png.Encode(os.Stdout, img) // NOTE: ignoring errors
+}
+
+func supersampling(img *image.RGBA) *image.RGBA {
+	r := image.NewRGBA(img.Bounds())
+	ymax := r.Bounds().Max.Y
+	xmax := r.Bounds().Max.X
+	for y := 0; y < ymax; y++ {
+		for x := 0; x < xmax; x++ {
+			x1 := x + 1
+			y1 := y + 1
+			if x == xmax-1 {
+				x1 = x - 1
+			}
+			if y == ymax-1 {
+				y1 = y - 1
+			}
+			r1, g1, b1, _ := img.At(x, y).RGBA()
+			r2, g2, b2, _ := img.At(x1, y).RGBA()
+			r3, g3, b3, _ := img.At(x, y1).RGBA()
+			r4, g4, b4, _ := img.At(x1, y1).RGBA()
+			R := uint8((r1 + r2 + r3 + r4) / 4)
+			G := uint8((g1 + g2 + g3 + g4) / 4)
+			B := uint8((b1 + b2 + b3 + b4) / 4)
+			c := color.RGBA{R, G, B, 0xff}
+			r.Set(x, y, c)
+		}
+	}
+	return r
 }
 
 func mandelbrot(z complex128) color.Color {
