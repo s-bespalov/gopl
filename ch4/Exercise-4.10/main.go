254507a74ad3ba351dec1efc0c32858b5b272bbd
Sergey Bespalov <sergey.bespalov@protonmail.com>
2020-10-14 11:29:12 +0300

    Exercise 4.10, finish

--- ch4/Exercise-4.10/main.go
+++ ch4/Exercise-4.10/main.go
@@ -2,21 +2,42 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/s-bespalov/gopl/ch4/Exercise-4.10/github"
 )
 
 func main() {
+	lastMonth := bytes.NewBufferString("Last month:\n")
+	lastYear := bytes.NewBufferString("Last year:\n")
+	older := bytes.NewBufferString("Older thar year:\n")
+
+	today := time.Now()
+	monthAgo := today.AddDate(0, -1, 0)
+	yearAgo := today.AddDate(-1, 0, 0)
+
 	result, err := github.SearchIssues(os.Args[1:])
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
 	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s %v\n",
+		var out *bytes.Buffer
+		if item.CreatedAt.After(monthAgo) {
+			out = lastMonth
+		} else if item.CreatedAt.After(yearAgo) {
+			out = lastYear
+		} else {
+			out = older
+		}
+		fmt.Fprintf(out, "#%-5d %9.9s %.55s %v\n",
 			item.Number, item.User.Login, item.Title, item.CreatedAt)
 	}
+	lastMonth.WriteTo(os.Stdout)
+	lastYear.WriteTo(os.Stdout)
+	older.WriteTo(os.Stdout)
 }
