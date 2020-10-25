efdeee412dc3c502947a724d4cf1cac1d85524c3
Sergey Bespalov <sergey.bespalov@protonmail.com>
2020-10-25 22:33:24 +0300

    Exercise 4.14, add missing file

---
+++ ch4/Exercise-4.14/github2/issues.go
@@ -0,0 +1,33 @@
+// Package github2 provides a Go API for Github repository tracker.
+package github2
+
+import "time"
+
+const IssueURL = "https://api.github.com/search/issues"
+
+type IssueSearchResult struct {
+	TotalCount int `json:"total_count"`
+	Items      []*Issue
+}
+
+type Issue struct {
+	Number    int
+	HTMLURL   string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	Milestone *Milestone
+	CreatedAt time.Time `json:"created_at"`
+	Body      string    // in Markdown format
+}
+
+type User struct {
+	Login   string
+	HTMLURL string `json:"html_url"`
+}
+
+type Milestone struct {
+	Number  int
+	Title   string
+	HTMLURL string `json:"html_url"`
+}
