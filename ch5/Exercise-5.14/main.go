4c487ffea44c6aaf949a5d6719b3407948a0d089
Sergey Bespalov <sergey.bespalov@protonmail.com>
2020-12-04 00:33:25 +0300

    Exercise 5.14

---
+++ ch5/Exercise-5.14/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+// prereqs maps computer science courses to their prerequisites.
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+func main() {
+	f := func(item string) []string {
+		fmt.Println(item)
+		return prereqs[item]
+	}
+	var keys []string
+	for key := range prereqs {
+		keys = append(keys, key)
+	}
+	breadthFirst(f, keys)
+}
