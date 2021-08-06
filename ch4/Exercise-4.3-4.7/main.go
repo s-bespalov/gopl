7f120536d201b47f0177c9bfeb246fe21e2d783c
Sergey Bespalov <sergey.bespalov@protonmail.com>
2021-08-06 11:46:57 +0300

    Exercise 4.4 rotate

--- ch4/Exercise-4.3-4.7/main.go
+++ ch4/Exercise-4.3-4.7/main.go
@@ -9,8 +9,33 @@ func reverse(s *[6]int) {
 	}
 }
 
+// Exercise-4.4
+// Rotates a slice left by n elements
+func rotate(s []int, n int) {
+	n = n % len(s)
+	buf := make([]int, n)
+	for i := 0; i < len(s); i++ {
+		if i < n {
+			buf[i] = s[i]
+		}
+		if i < len(s)-n {
+			s[i] = s[i+n]
+		} else {
+			s[i] = buf[i-(len(s)-n)]
+		}
+	}
+}
+
 func main() {
 	a := [...]int{0, 1, 2, 3, 4, 5}
 	reverse(&a)
-	fmt.Println(a)
+	fmt.Println("reversed", a)
+	b := [...]int{0, 1, 2, 3, 4, 5}
+	fmt.Printf("%v rotated by %d :\n", b, 2)
+	rotate(b[:], 2)
+	fmt.Printf("%v\n", b)
+	c := [...]int{0, 1, 2, 3, 4, 5}
+	fmt.Printf("%v rotated by %d :\n", c, 10)
+	rotate(c[:], 10)
+	fmt.Printf("%v\n", c)
 }
