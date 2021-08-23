7a6d223561c33f59a1a12979faadaca3c4af44e7
Sergey Bespalov <sergey.bespalov@protonmail.com>
2021-08-23 12:08:27 +0300

    Open Issue in External Editor Exercise 4.11

--- ch4/Exercise-4.11/main.go
+++ ch4/Exercise-4.11/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 	"time"
 
@@ -13,17 +15,21 @@ import (
 )
 
 const creditsFile = "credits"
-const tmpFolder = "tmp"
-const creditsPath = tmpFolder + "/" + creditsFile
+const tmpFolder = "tmp/"
+const creditsPath = tmpFolder + creditsFile
+const jsonIndent = "  "
+const jsonPrefix = ""
 
-var t string
-var u string
-var m string
+var token string
+var user string
+var mode string
+var editor string
 
 func init() {
-	flag.StringVar(&u, "u", "", "user name")
-	flag.StringVar(&t, "t", "", "github access token")
-	flag.StringVar(&m, "m", "search", "mode, search/read/update/create/close")
+	flag.StringVar(&user, "u", "", "user name")
+	flag.StringVar(&token, "t", "", "github access token")
+	flag.StringVar(&mode, "m", "", "mode, search/read/update/create/close")
+	flag.StringVar(&editor, "e", "geany", "editor, default nano")
 }
 
 func readCredits() {
@@ -33,13 +39,13 @@ func readCredits() {
 		return
 	}
 	d := strings.Split(string(data), "\n")
-	t = d[0]
-	u = d[1]
+	token = d[0]
+	user = d[1]
 }
 
 func saveCredits() {
 	os.Mkdir(tmpFolder, 0777)
-	data := fmt.Sprintf("%s\n%s", t, u)
+	data := fmt.Sprintf("%s\n%s", token, user)
 	err := ioutil.WriteFile(creditsPath, []byte(data), 0777)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "issue editor:", err)
@@ -48,21 +54,15 @@ func saveCredits() {
 }
 
 func read() {
-	if flag.Arg(0) == "" || flag.Arg(1) == "" || flag.Arg(2) == "" {
-		log.Fatalln("Arguments should have owner, repo, issue number. current:", flag.Args())
-	}
+	checkParams()
 	issue, err := github.ReadIssue(flag.Args())
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 	fmt.Printf("Number: #%d\nAuthor: %s\nTitle:%s\nBody:\n%s\n", issue.Number, issue.User.Login, issue.Title, issue.Body)
 }
 
 func search() {
 	result, err := github.SearchIssues(flag.Args())
-	if err != nil {
-		log.Fatal(err)
-	}
+	check(err)
 	fmt.Printf("%d issues:\n", result.TotalCount)
 	fmt.Println("less than a month old:")
 	for _, item := range result.Items {
@@ -85,28 +85,64 @@ func search() {
 	}
 }
 
+func update() {
+	checkParams()
+	issue, err := github.ReadIssue(flag.Args())
+	check(err)
+	f := fmt.Sprintf("%s%d%d.json", tmpFolder, time.Now().Unix(), issue.Number)
+	data, err := json.MarshalIndent(issue, jsonPrefix, jsonIndent)
+	check(err)
+	err = ioutil.WriteFile(f, data, 0644)
+	check(err)
+	openEditor(f)
+	fmt.Println("Editor closed")
+}
+
+func checkParams() {
+	if flag.Arg(0) == "" || flag.Arg(1) == "" || flag.Arg(2) == "" {
+		log.Fatalln("Arguments should have owner, repo, issue number. current:", flag.Args())
+	}
+}
+
+func openEditor(f string) {
+	e, err := exec.LookPath(editor)
+	check(err)
+	p, err := os.StartProcess(e, []string{e, f}, &os.ProcAttr{Env: os.Environ()})
+	check(err)
+	_, err = p.Wait()
+	check(err)
+}
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
 func main() {
 	flag.Parse()
-	if u == "" || t == "" {
-		tmpU, tmpT := u, t
+	if user == "" || token == "" {
+		tmpU, tmpT := user, token
 		readCredits()
 		if tmpT != "" {
-			t = tmpT
+			token = tmpT
 			saveCredits()
 		}
 		if tmpU != "" {
-			u = tmpU
+			user = tmpU
 			saveCredits()
 		}
 	} else {
 		saveCredits()
 	}
-	github.OAuth(u, t)
+	github.OAuth(user, token)
 
-	switch m {
+	switch mode {
 	case "read":
 		read()
 	case "search":
 		search()
+	case "update":
+		update()
 	}
 }
