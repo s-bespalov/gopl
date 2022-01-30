b58597514a282f692cd061c38dc49ef8628f3f14
Sergey Bespalov <sergey.bespalov@protonmail.com>
2022-01-30 22:14:37 +0300

    Exercise-5.8, done

--- ch5/Exercise-5.8/main.go
+++ ch5/Exercise-5.8/main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"com.github/s-bespalov/gopl/ch5/Exercise-5.8/search"
+	"golang.org/x/net/html"
+)
+
 func main() {
-	
+	url := os.Args[1]
+	id := os.Args[2]
+	resp, err := http.Get(url)
+	check(err)
+	if resp.StatusCode != 200 {
+		log.Fatalf("url: %s, status code not OK, status code:%d", url, resp.StatusCode)
+	}
+	h, err := html.Parse(resp.Body)
+	check(err)
+	n, err := search.ElsementByID(h, id)
+	check(err)
+	fmt.Printf("<%s", n.Data)
+	for _, a := range n.Attr {
+		fmt.Printf(" %s='%s'", a.Key, a.Val)
+	}
+	fmt.Print(">\n")
+}
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- ch5/Exercise-5.8/search/search.go
+++ ch5/Exercise-5.8/search/search.go
@@ -2,6 +2,36 @@ package search
 
 import "golang.org/x/net/html"
 
-func ElsementByID(n *html.Node, id string) (*html.Node, error) {
-	return nil, nil
+func ElsementByID(h *html.Node, id string) (*html.Node, error) {
+	var r *html.Node
+	forEachNode(h, func(n *html.Node) bool {
+		if n.Type == html.ElementNode {
+			for _, a := range n.Attr {
+				if a.Key == "id" && a.Val == id {
+					r = n
+					return false
+				}
+			}
+		}
+		return true
+	}, nil)
+	return r, nil
+}
+
+func forEachNode(n *html.Node, pre, post func(n *html.Node) bool) {
+	if pre != nil {
+		if !pre(n) {
+			return
+		}
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, pre, post)
+	}
+
+	if post != nil {
+		if !post(n) {
+			return
+		}
+	}
 }
