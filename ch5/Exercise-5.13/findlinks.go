18ad1fbd14cb80c5ec643957de07a1f3399c34b1
Sergey Bespalov <sergey.bespalov@protonmail.com>
2022-02-20 23:59:08 +0300

    exercise 5.13 done

--- ch5/Exercise-5.13/findlinks.go
+++ ch5/Exercise-5.13/findlinks.go
@@ -3,8 +3,16 @@ package main
 import (
 	"com/github/s-bespalov/gopl/ch5/Exercise-5.13/links"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 )
+
+const rootDir = "downloads"
 
 // breasthFirst calls f for each item in the worklist.
 // Any items returned by f are added to the worklist.
@@ -23,11 +31,50 @@ func breadthFirst(f func(item string) []string, worklist []string) {
 	}
 }
 
-func crawl(url string) []string {
-	fmt.Println(url)
-	list, err := links.Extract(url)
-	if err != nil {
-		log.Print(err)
+func crawlSites(worklist []string) {
+	saved := make(map[string]bool)
+	crawl := func(URL string) []string {
+		fmt.Println(URL)
+		list, err := links.Extract(URL)
+		if err != nil {
+			log.Print(err)
+		}
+		u, err := url.Parse(URL)
+		if err != nil {
+			return list
+		}
+		h := u.Hostname()
+		for _, URL = range list {
+			u2, err := url.Parse(URL)
+			if err != nil {
+				log.Print(err)
+				return list
+			}
+			if saved[URL] {
+				return list
+			}
+			if h != u2.Hostname() {
+				continue
+			}
+			resp, err := http.Get(URL)
+			if err != nil || resp.StatusCode != http.StatusOK {
+				return list
+			}
+			data, err := io.ReadAll(resp.Body)
+			if err != nil {
+				return list
+			}
+			h = strings.ReplaceAll(h, ".", "_")
+			p := fmt.Sprintf("%s/%s/%s", rootDir, h, u2.Path)
+			f := fmt.Sprintf("%s/%d_raw", p, time.Now().Unix())
+			err = os.MkdirAll(p, 0777)
+			if err != nil {
+				return list
+			}
+			os.WriteFile(f, data, 0666)
+			saved[URL] = true
+		}
+		return list
 	}
-	return list
+	breadthFirst(crawl, worklist)
 }
--- ch5/Exercise-5.13/main.go
+++ ch5/Exercise-5.13/main.go
@@ -3,5 +3,5 @@ package main
 import "os"
 
 func main() {
-	breadthFirst(crawl, os.Args[1:])
+	crawlSites(os.Args[1:])
 }
